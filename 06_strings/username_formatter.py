a4f494ee6deca5e00d356b08b7b416b6f7f2b0f9
Zuhriddin0931 <sirojiddinovzuhriddin977@gmail.com>
2026-06-09 15:26:26 +0300

    added simple username formatter

---
+++ 06_strings/username_formatter.py
@@ -0,0 +1,10 @@
+# Foydalanuvchidan username kiritishni so‘raymiz
+print("Username kiriting:")
+username = input()
+
+# Username-ni kichik harflarga o‘tkazamiz va oldiga @ qo‘shamiz
+formatted_username = "@" + username.lower()
+
+# Natijani chiqaramiz
+print("Sizning username-ingiz:")
+print(formatted_username)
